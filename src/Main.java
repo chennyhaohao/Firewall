ed0a68ce4d273d2303f01d340ba45db357d43f6e
chennyhaohao <chennyhaohao@gmail.com>
2018-09-21 05:25:01 +0400

    Merging tested

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) {
-		new Firewall("");
-		return;
+		Firewall f = new Firewall("");
+		int[] tests = {0, 2, 1, 4, 5, 7, 7, 8, 7, 7, 9, 12, 15, 17};
+		TreeSet<Range> s = new TreeSet<Range>();
+		for (int i=0; i<tests.length; i+=2) {
+			s.add(new Range(tests[i], tests[i+1]));
+		}
+		f.mergeRanges(s);
+		for (Range r: s) {
+			System.out.printf("%d %d\n", r.start, r.end);
+		}
 	}
 }
